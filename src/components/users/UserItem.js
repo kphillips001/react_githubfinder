971255586bbd69afaefa4b4bc2ed9562810155b8
Kevin <knphillips01@gmail.com>
2021-11-08 19:54:31 -0600

    refactored

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,50 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class UserItem extends Component {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     id: 'id',
-  //     login: 'mojombo',
-  //     avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-  //     html_url: 'https://github.com/mojombo',
-  //   };
-  // }
-
-  // state = {
-  //   id: 'id',
-  //   login: 'mojombo',
-  //   avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-  //   html_url: 'https://github.com/mojombo',
-  // };
-
-  render() {
-    // destructure this.state
-    // const { login, avatar_url, html_url } = this.state;
-    const { login, avatar_url, html_url } = this.props.user;
-
-    return (
-      <div className='card text-center'>
-        <img
-          // src={this.state.avatar_url} - destructure
-          src={avatar_url}
-          alt=''
-          className='round-img'
-          style={{ width: '60px' }}
-        />
-        {/* <h3>{this.state.login}</h3> - destructure */}
-        <div>
-          <h3>{login}</h3>
-          {/* <a href={this.state.html_url} className='btn btn-dark btn-sm my-1'>
-            More
-          </a> - destructure */}
-          <a href={html_url} className='btn btn-dark btn-sm my-1'>
-            More
-          </a>
-        </div>
+// arrow function
+// Functiona components - no longer use this keyword, will pass in props
+// destructure props
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className='card text-center'>
+      <img
+        src={avatar_url}
+        alt=''
+        className='round-img'
+        style={{ width: '60px' }}
+      />
+      <div>
+        <h3>{login}</h3>
+        <a href={html_url} className='btn btn-dark btn-sm my-1'>
+          More
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired,
+};
 
 export default UserItem;
