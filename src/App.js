d04d1749aeb113236da6e3d597f7b0d5758b7c69
Kevin <knphillips01@gmail.com>
2021-11-09 11:54:23 -0600

    completed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 
 import './App.css';
 
 class App extends Component {
+  // We want to put the users that come back into our state.
+  // State is empty at first
+  // Cannot directly change state. Have to use this.SetState
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  // lifecycle method
+  async componentDidMount() {
+    // Cannot directly change state. Have to use this.SetState
+    // Setting state (loading) to true
+    this.setState({ loading: true });
+
+    // Axios deals with promises with .then (will get a response)
+    // Create a variable res to await the request
+    const res = await axios.get('https://api.github.com/users');
+    // .then((res) => console.log(res.data));
+    // will use async/await instead of .then
+
+    // After we get the request and get the response, we want to reset the state. Set users to res.data that we get from the api, then set loading back to false.
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
     return (
       <div className='App'>
         <Navbar title='Github Finder' icon='fab fa-github' />
         <div container='container'>
-          <Users />
+          {/*Now that we have the users in state, we want to pass them down into the Users component through props. */}
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,28 +2,29 @@ import React, { Component } from 'react';
 import UserItem from './UserItem';
 
 class Users extends Component {
-  state = {
-    users: [
-      {
-        id: 'id',
-        login: 'mojombo',
-        avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo',
-      },
-      {
-        id: 'id',
-        login: 'defunkt',
-        avatar_url: 'https://avatars.githubusercontent.com/u/2?v=4',
-        html_url: 'https://api.github.com/users/defunkt',
-      },
-      {
-        id: 'id',
-        login: 'pjhyett',
-        avatar_url: 'https://avatars.githubusercontent.com/u/3?v=4',
-        html_url: 'https://api.github.com/users/pjhyett',
-      },
-    ],
-  };
+  // No longer need this state.
+  // state = {
+  //   users: [
+  //     {
+  //       id: 'id',
+  //       login: 'mojombo',
+  //       avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
+  //       html_url: 'https://github.com/mojombo',
+  //     },
+  //     {
+  //       id: 'id',
+  //       login: 'defunkt',
+  //       avatar_url: 'https://avatars.githubusercontent.com/u/2?v=4',
+  //       html_url: 'https://api.github.com/users/defunkt',
+  //     },
+  //     {
+  //       id: 'id',
+  //       login: 'pjhyett',
+  //       avatar_url: 'https://avatars.githubusercontent.com/u/3?v=4',
+  //       html_url: 'https://api.github.com/users/pjhyett',
+  //     },
+  //   ],
+  // };
 
   // Want to loop through users and create a list
   // Takes in a function. Will use a callback function
@@ -31,7 +32,8 @@ class Users extends Component {
   render() {
     return (
       <div style={userStyle}>
-        {this.state.users.map((user) => (
+        {/* Change this.state to this.props as users are coming in as props */}
+        {this.props.users.map((user) => (
           // <div key={user.id}>{user.login}</div>
           // Pass user in as a prop
           <UserItem key={user.id} user={user} />
